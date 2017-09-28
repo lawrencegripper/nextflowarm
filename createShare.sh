909af2b62e480ba9ea342b83f67fce92142524d8
Lawrence <info@grippers.co.uk>
2017-09-28 13:32:22 +0100

    Fixup script bugs

--- createShare.sh
+++ createShare.sh
@@ -5,10 +5,10 @@
 # $3 = Azure file share name
 
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ wheezy main" | \
-     sudo tee /etc/apt/sources.list.d/azure-cli.list
+     sudo tee /etc/apt/sources.list.d/azure-cli.list 
 
 apt-key adv --keyserver packages.microsoft.com --recv-keys 417A0893
-apt-get install apt-transport-https
-apt-get update && sudo apt-get install azure-cli
+apt-get install apt-transport-https -y
+apt-get update -y && sudo apt-get install azure-cli -y
 
 az storage share create --name $3 --quota 2048 --connection-string "DefaultEndpointsProtocol=https;EndpointSuffix=core.windows.net;AccountName=$1;AccountKey=$2" >> /tmp/sharecreate.log
